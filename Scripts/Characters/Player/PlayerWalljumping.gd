e2c2e13003c0ff5b7dd3fe196c5e3110c38fd08f
rockrick1 <henriquecerquinho@gmail.com>
2024-01-31 18:58:44 -0300

    feat: add dynamic air control do walljump and fix clamp bug

--- Scripts/Characters/Player/PlayerWalljumping.gd
+++ Scripts/Characters/Player/PlayerWalljumping.gd
@@ -1,10 +1,15 @@
 extends PlayerMovementState
 
-const AIR_CONTROL := 2
+@export var start_air_control := 2
+@export var end_air_control := 5
+@export var air_control_transition_time := .5
+
+var state_time := .0
 
 func enter():
 	animator.set("parameters/ground_air_transition/transition_request", "air")
 	player.snap_vector = Vector3.ZERO
+	state_time = 0
 
 func physics_process(delta):
 	var move_direction := Vector3.ZERO
@@ -13,12 +18,17 @@ func physics_process(delta):
 	move_direction = move_direction.rotated(Vector3.UP, spring_arm_pivot.rotation.y).normalized()
 
 	player.velocity.y -= player.gravity * delta
+	state_time += delta
+	
+	var air_control = lerp(start_air_control, end_air_control, state_time / air_control_transition_time)
+	air_control = clamp(air_control, start_air_control, end_air_control)
+	print(air_control)
 
 	var h_speed := cc.run_speed
-	player.velocity.x += move_direction.x * h_speed * delta * AIR_CONTROL
-	player.velocity.x = clamp(-h_speed, player.velocity.x, h_speed)
-	player.velocity.z += move_direction.z * h_speed * delta * AIR_CONTROL
-	player.velocity.z = clamp(-h_speed, player.velocity.z, h_speed)
+	player.velocity.x += move_direction.x * h_speed * delta * air_control
+	player.velocity.x = clamp(player.velocity.x, -h_speed, h_speed)
+	player.velocity.z += move_direction.z * h_speed * delta * air_control
+	player.velocity.z = clamp(player.velocity.z, -h_speed, h_speed)
 	
 	if move_direction:
 		player.player_mesh.rotation.y = lerp_angle(player.player_mesh.rotation.y, atan2(player.velocity.x, player.velocity.z), player.LERP_VALUE)
